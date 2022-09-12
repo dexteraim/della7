7867f73662dc97b33b2d67ffb3a12374f908d5a0
aimdexter <draimade@gmail.com>
2022-09-12 01:53:16 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Navbar } from "./components/Navbar";
-import { Header } from "./components/Header";
-import { Projects } from "./components/Projects";
-import { Team } from "./components/Team";
-import { Tasks } from "./components/Tasks";
-import { Footer } from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Team from "./components/Team";
+import Tasks from "./components/Tasks";
+import Footer from "./components/Footer";
 
 function App() {
   return (
