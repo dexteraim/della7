2109898ffa1696d4033a1fdd71deb806e418ca3c
aimdexter <draimade@gmail.com>
2022-09-12 03:51:42 +0100

    Clients carousel done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,29 +1,44 @@
 import Slider from 'infinite-react-carousel';
 
 const Clients = () => {
+  const client = [
+    {
+      clientName: '/images/clients/Google.png',
+    },
+    {
+      clientName: '/images/clients/Lenovo.png',
+    },
+    {
+      clientName: '/images/clients/Paypal.png',
+    },
+    {
+      clientName: '/images/clients/Samsung.png',
+    },
+    {
+      clientName: '/images/clients/Shopify.png',
+    },
+    {
+      clientName: '/images/clients/Slack.png',
+    },
+  ];
+
   const settings2 = {
     arrows: false,
     autoplay: true,
     duration: 100,
-    slidesToShow: 5,
+    slidesToShow: 2,
   };
   return (
-    <>
-      <h1 className="tex-[26px] text-center font-semibold text-primary-blue">
-        Our clients
-      </h1>
-      <div className="flex flex-col justify-center text-center text-xs font-semibold lg:hidden">
+    <div className="pb-[66px]">
+      <h1 className="tex-[26px] text-center font-semibold text-primary-blue">Our clients</h1>
+      <div className="flex flex-col justify-center lg:hidden">
         <Slider {...settings2}>
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
-          <img src="/images/carousel/dot-svgrepo-com.svg" alt="" className="h-1 w-1" />
+          {client.map(({ clientName }) => {
+            return <img src={clientName} alt="" className="h-[112px] w-[232.4px]" />;
+          })}
         </Slider>
       </div>
-    </>
+    </div>
   );
 };
 
